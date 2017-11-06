774f1041fdd2fb879c159bc55ea86ed18a37f7ff
xingjiudong <25635680@qq.com>
2017-11-06 18:31:01 +0800

    Fix env

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
 
-export TIMEOUT_CONNECT=5s
-export TIMEOUT_CLIENT=50s
-export TIMEOUT_SERVER=50s
-export TIMEOUT_HTTP_KEEP_ALIVE=50s
-export MAXCONN=2000
+export TIMEOUT_CONNECT=${TIMEOUT_CONNECT:-5s}
+export TIMEOUT_CLIENT=${TIMEOUT_CLIENT:-50s}
+export TIMEOUT_SERVER=${TIMEOUT_SERVER:-50s}
+export TIMEOUT_HTTP_KEEP_ALIVE=${TIMEOUT_HTTP_KEEP_ALIVE:-50s}
+export MAXCONN=${MAXCONN:-2000}
 
 # Make sure service is running
 service rsyslog start
